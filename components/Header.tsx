89e9975224dd7d9f3a8236c1396b8dd26c81c1ec
danial137 <danial79fakhrabadi@gmail.com>
2026-05-24 00:25:01 +0330

    feat: add Header component and UI elements with Tailwind CSS
    - Updated package.json to include new dependencies: class-variance-authority, clsx, lucide, lucide-react, radix-ui, shadcn, tailwind-merge, and tw-animate-css.
    - Created components.json for Shadcn configuration.
    - Implemented Header component with search input and cart badge.
    - Added utility function `cn` for class name merging.
    - Developed Badge component with variants using class-variance-authority.
    - Created Button component with multiple variants and sizes.

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import { Badge } from "@/shared/ui/badge"
+import { Package, Search, ShoppingCart } from "lucide-react"
+import Link from 'next/link'
+
+const Header = () => {
+    return (
+        <header className='sticky top-0 z-10 backdrop-blur w-full flex justify-between py-6 px-20 border-b border-athens-gray'>
+            <Link href='/' className='flex gap-2'>
+
+                <Package className="text-red-500" />
+                <span>TechStore</span>
+
+            </Link>
+
+            <div className="flex gap-4 items-center">
+                <div className="relative">
+                    <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-santas-gray  " />
+                    <input className="outline-none w-80 border border-athens-gray py-2 pl-8 rouded-md text-sm" placeholder="search products ... " />
+                </div>
+                <Link href="/cart" className="relative">
+                    <ShoppingCart className="h-5 w-5" />
+                    <Badge>5</Badge>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header
